0e119accf5c951c24f754f106a865642966fab51
Hans Moog <hm@mkjc.net>
2019-11-12 15:38:42 +0100

    Refactor: removed objectstorage from repo

--- packages/objectstorage/parameters.go
+++
@@ -1,5 +0,0 @@
-package objectstorage
-
-import "github.com/iotaledger/goshimmer/packages/parameter"
-
-var DIRECTORY = parameter.AddString("OBJECTSTORAGE/DIRECTORY", "objectsdb", "path to the database folder")
